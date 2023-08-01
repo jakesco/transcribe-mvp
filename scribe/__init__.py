47c5075e7062fbab1df628d7f6d539f25b9f1653
Jake Scott <jscott@roguecu.org>
2023-08-01 14:39:28 -0700

    minimal htmx form submission

--- scribe/__init__.py
+++ scribe/__init__.py
@@ -1,19 +1,25 @@
 from typing import Annotated
 from fastapi import FastAPI, Request, File, Form, UploadFile
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from .config import app_settings
 
 settings = app_settings()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.post("/transcribe")
-async def transcribe(file: Annotated[UploadFile, File()]):
-    if not file:
-        return {"message": "File not uploaded"}
-    else:
-        return {"filename": file.filename}
 
-# This needs to be defined last to not shadow other endpoints
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/")
+async def transcribe(request: Request, file: Annotated[UploadFile, File()]):
+    message = f"{file.filename} uploaded" if file else "File not uploaded"
+    return templates.TemplateResponse("messages.html", {"request": request, "message": message})
+
